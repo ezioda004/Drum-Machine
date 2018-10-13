67a47eaaf5097826c6ee044f29158094f798eb37
Aditya Verma <adi004.zz@gmail.com>
2018-10-14 02:41:03 +0530

    feat: added styles to the button and display

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -4,8 +4,16 @@ class Display extends Component {
   render() {
     return (
       <div id="display">
-        <h1>Drum Machine</h1>
-        <h2>{this.props.name}</h2>
+        <h1>
+          <span className="first-word">Drum</span>{" "}
+          <span className="second-word">Machine</span>
+        </h1>
+        <div className="screen">
+            <div className = "inside">
+                <h2>{this.props.name}</h2>
+            </div>
+          
+        </div>
       </div>
     );
   }
